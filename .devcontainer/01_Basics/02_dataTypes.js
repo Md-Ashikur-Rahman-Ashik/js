67a6a05bf04845043b96bc5a2b3e49a0b78f4c71
MD ASHIKUR RAHMAN ASHIK <152707760+Md-Ashikur-Rahman-Ashik@users.noreply.github.com>
2023-12-03 06:19:51 +0000

    data types done

--- .devcontainer/01_Basics/02_dataTypes.js
+++ .devcontainer/01_Basics/02_dataTypes.js
@@ -1,4 +1,21 @@
-"use strict";
+let score = "33"
 
-// alert("Hello")
+// console.log(typeof score);
 
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+
+let isLoggedIn = 1
+
+let ConverIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(ConverIsLoggedIn);
+
+let aNumber = 55
+
+let stringNumber = String(aNumber)
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
