47076a59568eb04ca44a0fb5e6dc871996d0568c
MD ASHIKUR RAHMAN ASHIK <152707760+Md-Ashikur-Rahman-Ashik@users.noreply.github.com>
2023-12-03 12:48:30 +0000

    new lesson learned

--- 01_Basics/04_dataTypesSummary.js
+++ 01_Basics/04_dataTypesSummary.js
@@ -1,8 +1,14 @@
 let oneSymbol = Symbol("123")
 let anotherSymbol = Symbol("123")
 
-console.log(oneSymbol === anotherSymbol);
+// console.log(oneSymbol === anotherSymbol);
 
 const myFunction = function(){
-    console.log("Hello World");
+    // console.log("Hello World");
 }
+
+
+let myName = "Ashik"
+let myAge = "18"
+
+console.log(`My name is ${myName} and my age is ${myAge}`);
