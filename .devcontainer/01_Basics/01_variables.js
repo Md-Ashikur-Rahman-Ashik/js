39fe8337f70c61b96c5c615bb44d5e69580aedbe
MD ASHIKUR RAHMAN ASHIK <152707760+Md-Ashikur-Rahman-Ashik@users.noreply.github.com>
2023-12-03 02:27:40 +0000

    two lessons done partialy

---
+++ .devcontainer/01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 11
+let accountEmail = "ashik@google.com"
+var accountPassword = "12345"
+accountCity = "Pur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+/*
+    Prefer not to use var
+*/
+accountEmail = "hc@hc.com"
+accountPassword = "Montu"
+accountCity = "Cumilla"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
