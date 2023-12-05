94c9f8222a545d316e35d9f9f3b5a0391d7182e3
MD ASHIKUR RAHMAN ASHIK <152707760+Md-Ashikur-Rahman-Ashik@users.noreply.github.com>
2023-12-05 04:52:28 +0000

    Third Day Done

--- 02_Basics/01_thirdDay.js
+++ 02_Basics/01_thirdDay.js
@@ -29,8 +29,79 @@ const newArray = myArray.join()
 
 const otherArray = myArray.slice(1, 3)
 
-console.log(otherArray);
-console.log("B ", myArray);
+// console.log(otherArray);
+// console.log("B ", myArray);
 
 const anotherArray = myArray.splice(1, 3)
-console.log(anotherArray);
+// console.log(anotherArray);
+
+const marvelHeros = ["Thor", "IronMan", "SpiderMan"]
+const dcHeros = ["Superman", "Flash", "Batman"]
+
+// marvelHeros.push(dcHeros)
+
+// console.log(marvelHeros);
+
+
+
+// const allHeros = marvelHeros.concat(dcHeros)
+
+// console.log(allHeros);
+
+const allNewHeros = [...marvelHeros, ...dcHeros]
+
+// console.log(allNewHeros);
+
+const anotherNewArray = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+const realArray = anotherNewArray.flat(Infinity)
+
+// console.log(realArray);
+
+// console.log(Array.isArray("Ashik"));
+
+// console.log(Array.from("Ashik"));
+
+// console.log(Array.from({name: "Ashik"}))
+
+let scoreOne = 100
+let scoreTwo = 200
+let scoreThree = 300
+
+// console.log(Array.of(scoreOne, scoreTwo, scoreThree));
+
+const mySymbol = Symbol("keyOne")
+
+const jsUser = {
+    name: "Ashik",
+    "full name": "Ashikur Rahman",
+    age: 18,
+    [mySymbol]: "mykeyOne",
+    location: "Cumilla",
+    email: "md.ashikur.upwork@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(jsUser["email"]);
+// console.log(jsUser[mySymbol]);
+
+// myAnotherArray = ["H", "i"]
+
+jsUser.email = "ashik@chatgpt.com"
+
+// Object.freeze(jsUser)
+
+jsUser.email = "ashik.microsoft.com"
+
+// console.log(jsUser);
+
+jsUser.greeting = function(){
+    console.log("Hello Js User");
+}
+
+jsUser.greetingTwo = function(){
+    console.log(`Helo Js User, ${this.name}`);
+}
+
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
