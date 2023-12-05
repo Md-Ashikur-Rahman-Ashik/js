1188ba0dc45fd2a67e2e102d131823bc568e8151
MD ASHIKUR RAHMAN ASHIK <152707760+Md-Ashikur-Rahman-Ashik@users.noreply.github.com>
2023-12-05 11:06:57 +0000

    Learning Json

--- 02_Basics/02_fourthDay.js
+++ 02_Basics/02_fourthDay.js
@@ -21,7 +21,58 @@ const regularUser = {
 
 const objectOne = {1: "a", 2: "b"}
 const objectTwo = {3: "a", 4: "b"}
+const objectFour = {5: "a", 6: "b"}
 
-const objectThree = {objectOne, objectTwo}
+// const objectThree = {objectOne, objectTwo}
+// const objectThree = Object.assign({}, objectOne, objectTwo, objectFour)
 
-console.log(objectThree);
+const objectThree = {...objectOne, ...objectTwo, ...objectFour}
+
+// console.log(objectThree);
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 2,
+        email: "y@gmail.com"
+    },
+    {
+        id: 3,
+        email: "z@gmail.com"
+    }
+]
+
+users[1].email
+
+// console.log(ordheekUser);
+// console.log(Object.keys(ordheekUser));
+// console.log(Object.values(ordheekUser));
+// console.log(Object.entries(ordheekUser));
+// console.log(ordheekUser.hasOwnProperty("isLoggedIn"));
+
+const course = {
+    courseName: "Js in Hindi",
+    price: "999",
+    courseInstructor: "Hitesh"
+}
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+// console.log(instructor);
+
+// {
+//     "name": "Ashik",
+//     "courseName": "Js in Hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
