802c1fc311c6bec3edd56df1f0dd2549310dd7f8
MD ASHIKUR RAHMAN ASHIK <152707760+Md-Ashikur-Rahman-Ashik@users.noreply.github.com>
2023-12-05 09:47:27 +0000

    Understanding of object done

--- 02_Basics/02_fourthDay.js
+++ 02_Basics/02_fourthDay.js
@@ -5,4 +5,23 @@ ordheekUser.id = "123abc"
 ordheekUser.name = "Sammy"
 ordheekUser.isLoggedIn = false
 
-console.log(ordheekUser);
+// console.log(ordheekUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullName: {
+        userFullName: {
+            firstName: "Ashikur",
+            lastName: "Rahman"
+        }
+    }
+}
+
+// console.log(regularUser.fullName.userFullName.firstName);
+
+const objectOne = {1: "a", 2: "b"}
+const objectTwo = {3: "a", 4: "b"}
+
+const objectThree = {objectOne, objectTwo}
+
+console.log(objectThree);
