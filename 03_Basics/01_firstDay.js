5474270b1f7ff12793dcdb0139eedea9be019008
MD ASHIKUR RAHMAN ASHIK <152707760+Md-Ashikur-Rahman-Ashik@users.noreply.github.com>
2023-12-07 08:44:31 +0000

    Learning Functions

---
+++ 03_Basics/01_firstDay.js
@@ -0,0 +1,29 @@
+function sayMyName(){
+    console.log("A");
+    console.log("S");
+    console.log("H");
+    console.log("I");
+    console.log("K");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+    // let result = number1 + number2
+    // console.log("Ashik");
+    // return result
+    return number1 + number2
+    
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result:", result);
+
+function loginUserMessage(username){
+    return `${} just logged in`
+}
