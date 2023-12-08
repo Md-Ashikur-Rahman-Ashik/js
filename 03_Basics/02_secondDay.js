35ff4f4488e580b3fa22f443515963c1f0a3b826
MD ASHIKUR RAHMAN ASHIK <152707760+Md-Ashikur-Rahman-Ashik@users.noreply.github.com>
2023-12-08 10:43:12 +0000

    Learning Scope Level

--- 03_Basics/02_secondDay.js
+++ 03_Basics/02_secondDay.js
@@ -29,14 +29,21 @@ function returnSecondValue(getArray){
 // console.log(returnSecondValue(myArray));
 // console.log(returnSecondValue([200, 400, 500, 1000]))
 
+// var c = 300
 
+let a = 300
 
 if(true){
     let a = 10
     const b = 20
-    var c = 30
+    console.log("Inner:", a);
 }
 
-console.log(a);
-console.log(b);
-console.log(c);
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const userName = "Ashik"
+}
