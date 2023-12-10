f1c9e269e08cb0f01ba58320bc83823de74e23bd
MD ASHIKUR RAHMAN ASHIK <152707760+Md-Ashikur-Rahman-Ashik@users.noreply.github.com>
2023-12-10 11:04:20 +0000

    Resuming Javascript

---
+++ 03_Basics/03_thirdDay.js
@@ -0,0 +1,11 @@
+// (function chai(){
+//     console.log(`DB CONNECTED`);
+// })();
+
+// ( () => {
+//     console.log(`DB CONNECTED`);
+// })();
+
+( (name) => {
+    console.log(`DB CONNECTED TO ${name}`);
+})("Ashik");
