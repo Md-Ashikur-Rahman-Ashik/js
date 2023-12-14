3015d4958ae83c04a2a6c1267b3c14769de0bb73
MD ASHIKUR RAHMAN ASHIK <152707760+Md-Ashikur-Rahman-Ashik@users.noreply.github.com>
2023-12-14 12:23:29 +0000

    Still Learning Control Flow

--- 04_controlFlow/firstDay.js
+++ 04_controlFlow/firstDay.js
@@ -1,5 +1,17 @@
-const isUserLoggedIn = true
+// const isUserLoggedIn = true
+// const temperature = 41
 
-if ( 2 == "2"){
-    console.log("Executed");
+// if ( temperature === 41){
+//     console.log("Less than 50");
+// }
+
+// else {
+//     console.log("Temperature is greater than 50")
+// }
+
+const score = 200
+
+if ( score > 100 ){
+    const power = "Fly"
+    console.log(`User power: ${power}`);
 }
