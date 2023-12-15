40e9adc57f7882ce1d7d23a7d3d226aae7dee152
MD ASHIKUR RAHMAN ASHIK <152707760+Md-Ashikur-Rahman-Ashik@users.noreply.github.com>
2023-12-15 14:14:45 +0000

    Learning Control Flow for the second day

--- 04_controlFlow/firstDay.js
+++ 04_controlFlow/firstDay.js
@@ -9,9 +9,33 @@
 //     console.log("Temperature is greater than 50")
 // }
 
-const score = 200
+// const score = 200
 
-if ( score > 100 ){
-    const power = "Fly"
-    console.log(`User power: ${power}`);
+// if ( score > 100 ){
+//     let power = "Fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+// const balance = 1000
+
+// // if ( balance > 500 ) console.log("Test"),
+// // console.log("secondTest");
+
+// if ( balance < 500 ){
+//     console.log("Less than 500");
+// } else if ( balance < 750 ){
+//     console.log("Less than 750");
+// } else if ( balance < 900 ){
+//     console.log("Less than 900");
+// } else {
+//     console.log("Less than 1200")
+// }
+
+const userLoggedIn = true
+const debitCard = true
+
+if ( userLoggedIn && debitCard && 2 == 2 ){
+    console.log("Allowed to buy course");
 }
