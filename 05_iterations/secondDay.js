55a86ae6659a3cb791e46f2d4d1b3e4ab9666991
MD ASHIKUR RAHMAN ASHIK <152707760+Md-Ashikur-Rahman-Ashik@users.noreply.github.com>
2023-12-18 08:02:47 +0000

    Yet Learning High Order

---
+++ 05_iterations/secondDay.js
@@ -0,0 +1,9 @@
+const coding = ["Js", "Ruby", "Java", "Python", "C++"]
+
+// coding.forEach( function(item){
+//     console.log(item);
+// } )
+
+coding.forEach( (item) => {
+    console.log(item);
+} )
