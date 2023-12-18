f78ca12c7dd6762f0a59ae5cbccd1e2b2240091f
MD ASHIKUR RAHMAN ASHIK <152707760+Md-Ashikur-Rahman-Ashik@users.noreply.github.com>
2023-12-18 11:57:58 +0000

    Learning JS Dom

--- 05_iterations/secondDay.js
+++ 05_iterations/secondDay.js
@@ -97,9 +97,34 @@ const newNums = myNumbers
 
 const myNums = [1, 2, 3]
 
-const myTotal = myNums.reduce(function(acu, currval){
-    console.log(`acu:${acu} and currval:${currval}`);
-    return acu + currval
-}, 0)
+// const myTotal = myNums.reduce(function(acu, currval){
+//     console.log(`acu:${acu} and currval:${currval}`);
+//     return acu + currval
+// }, 0)
 
-console.log(myTotal);
+const myTotal = myNums.reduce( (acc, currval) => acc + currval, 0 )
+
+// console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: "Js course",
+        price: 2999
+    },
+    {
+        itemName: "Py course",
+        price: 999
+    },
+    {
+        itemName: "Mobile Dev course",
+        price: 5999
+    },
+    {
+        itemName: "Data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce( (acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
